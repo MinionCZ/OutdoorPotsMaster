5d2f48ae0ec32277413752c1f79ccc02271b8e80
MinionCZECH <43399650+MinionCZ@users.noreply.github.com>
2020-05-01 12:03:12 +0200

    Communication done with slave

--- src/Communication/Communication.cpp
+++ src/Communication/Communication.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+#include "DataClass.h"
 #include "LedController/LedController.h"
 #define TIME_OUT 5000
 uint32_t lastPeriodMillis = millis();
@@ -18,7 +19,7 @@ bool checkHeader(String command) {
 String readSerial() {
   String s = "";
   while (Serial.available() != 0) {
-    s += Serial.read();
+    s += (char)Serial.read();
   }
   return s;
 }
@@ -29,8 +30,10 @@ void isConnected() {
     stage = -1;
   }
   if (Serial.available() == 5) {
-    connected = checkHeader(readSerial());
+    String s = readSerial();
+    connected = checkHeader(s);
     if (connected) {
+      toggleBlinkingLeds(false);
       stage++;
     } else {
       stage = -1;
@@ -38,27 +41,42 @@ void isConnected() {
   }
 }
 
-double getVoltage(double lastVoltage){
-if(failedAttempts>2000){
-  stage++;
+double getVoltage(double lastVoltage) {
+  if (failedAttempts > 2000) {
+    stage++;
+  }
+  if (Serial.available() == 4) {
+    stage++;
+    String volts = readSerial();
+    volts.remove(3, 1);
+    double voltage = volts.toInt() / 100.0;
+    return voltage;
+  }
+  failedAttempts++;
+  return lastVoltage;
 }
-if(Serial.available() == 3){
-  stage++;
-  String volts = readSerial();
-  double voltage = volts.toInt() / 100.0;
-  return voltage;
+bool getBoolValue(bool lastStatus) {
+  if (failedAttempts > 2000) {
+    stage++;
+    return lastStatus;
+  }
+  if (Serial.available() == 2) {
+    String bools = readSerial();
+    stage++;
+    if (bools.charAt(0) == '1') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  failedAttempts++;
+  return lastStatus;
 }
-failedAttempts++;
-return lastVoltage;
-}
-
-
-
-
-
 
 void periodicGetCommands() {
   if (abs(millis() - lastPeriodMillis) > duration) {
+    DataClass* p = DataClass::getInstance();
+    DataClass data = *p;
     switch (stage) {
       case 0:
         Serial.print("#ping");
@@ -69,17 +87,48 @@ void periodicGetCommands() {
         isConnected();
         break;
       case 2:
+        data.setConnected(connected);
         Serial.print("#pinggb");
         stage++;
         failedAttempts = 0;
         break;
-      case 3: // to do
+      case 3:  // to do
+        data.setBatteryVoltage(getVoltage(data.getBatteryVoltage()));
         break;
-
-
+      case 4:
+        Serial.print("#pinggs");
+        stage++;
+        failedAttempts = 0;
+        break;
+      case 5:
+        data.setSolarPanelVoltage(getVoltage(data.getSolarPanelVoltage()));
+        break;
+      case 6:
+        Serial.print("#pinggc");
+        stage++;
+        failedAttempts = 0;
+        break;
+      case 7:
+        data.setCharging(getBoolValue(data.isCharging()));
+        break;
+      case 8:
+        Serial.print("#pinggw");
+        stage++;
+        failedAttempts = 0;
+        break;
+      case 9:
+        data.setWaterLevel(getBoolValue(data.isWaterLevelOk()));
+        stage++;
+        break;
+      case 10:
+        lastPeriodMillis = millis();
+        break;
       default:
+        lastPeriodMillis = millis();
         toggleBlinkingLeds(!connected);
         break;
     }
+  } else {
+    stage = 0;
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include "LedController/LedController.h"
 #include "Communication/Communication.h"
+#include "Communication/DataClass.h"
+DataClass* d;
 void setup() {
   // put your setup code here, to run once:
   initLedController();
   initRFCommunication();
+  d = DataClass::getInstance();
 }
-int counter = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
   blinkLeds();

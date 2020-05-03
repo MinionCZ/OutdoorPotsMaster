f4aaae43d395249298ec795ecfc4191e53d00da5
MinionCZECH <43399650+MinionCZ@users.noreply.github.com>
2020-05-03 13:28:18 +0200

    wifi works

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -28,37 +28,48 @@ void eraseEEPROM() {
   EEPROM.commit();
   server.send(200, "text/plain", "erased");
 }
+
+void wifiBegin() {
+  EEPROM.get(10, wifi);
+  char wifiName[wifi.ssid.length()];
+  char wifiPassword[wifi.password.length()];
+  for (uint16_t i = 0; i < wifi.ssid.length(); i++){
+    wifiName[i] = wifi.ssid.charAt(i);
+  }
+  for (uint16_t i = 0; i < wifi.password.length(); i++) {
+    wifiPassword[i] = wifi.password.charAt(i);
+  }
+  WiFi.begin(wifiName, wifiPassword);
+ }
+
 void wifiSetup() {
   WiFi.disconnect();
-  IPAddress ip(10, 20, 30, 69);
+  WiFi.softAPdisconnect();
+  IPAddress ip(10, 0, 0, 47);
   IPAddress gateway(10, 0, 0, 138);
   IPAddress subnet(255, 255, 255, 0);
   WiFi.config(ip, gateway, subnet, gateway);
   EEPROM.begin(512);
-  eraseEEPROM();
   if (EEPROM.read(0) == 1) {  // written in EEPROM
-    EEPROM.get(10, wifi);
-    WiFi.begin(wifi.ssid, wifi.password);
+    WiFi.mode(WIFI_STA);
+    wifiBegin();
     while (WiFi.status() != WL_CONNECTED) {
       delay(2);
+      if (WiFi.status() == 1){
+        break;
+      }
     }
     if(!WiFi.isConnected()){
       WiFi.softAPConfig(ip, gateway, subnet);
       WiFi.softAP("Garden", "123456789");
+      WiFi.mode(WIFI_AP);
     }
-  }
-  else {  // not yet written in EEPROM
+  } else {  // not yet written in EEPROM
     WiFi.softAPConfig(ip, gateway, subnet);
     WiFi.softAP("Garden", "123456789");
   }
 }
 void connectToWifi() {  // POST
-
-  if (WiFi.isConnected()) {
-    WiFi.disconnect();
-  } else {
-    WiFi.softAPdisconnect();
-  }
   wifiSetup();
   if(WiFi.isConnected()){
     server.send(204, "text/plain", "connected");
